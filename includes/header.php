c497a3f452ccfd73fbcaa0e841ba5b6baba99a79
MENDOZAAN <nicko1017mendoza@gmail.com>
2025-05-01 19:45:22 -0500

    -

--- includes/header.php
+++ includes/header.php
@@ -84,103 +84,37 @@
                                 <a href="<?= BASE_URL ?>/views/admin"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
                             </div>
                             <div class="nav-item">
-                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-user"></i><span>Usuarios</span> <span
-                                        class="badge badge-success"></span></a>
-                            </div>
-                            git checkout -b nico
-                            <!-- <div class="nav-item ">
-                                <a href="<?= BASE_URL ?>/views/listaRoles.php"><i class="ik ik-lock"></i><span>Roles</span></a>
+                                <a href="<?= BASE_URL ?>/views/admin/usuario.php"><i class="ik ik-users"></i><span>Usuarios</span></a>
                             </div>
                             <div class="nav-item">
-                                <a href="<?= BASE_URL ?>/views/usuarios.php"><i class="ik ik-user"></i><span>Usuarios</span> <span
-                                    class="badge badge-success"></span></a>
-                            </div>                        -->
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-shield"></i><span>Roles / Permisos</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-user-check"></i><span>Clientes</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-monitor"></i><span>Equipos ingresados</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-activity"></i><span>Estados de reparación</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-file-text"></i><span>Guía de ingreso/salida</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-clock"></i><span>Historial de atención</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-pie-chart"></i><span>Reportes</span></a>
+                            </div>
+                            <div class="nav-item">
+                                <a href="<?= BASE_URL ?>/views/admin/user.php"><i class="ik ik-globe"></i><span>Página pública de consulta</span></a>
+                            </div>
                         </nav>
-                    </div>
-                </div>
-            </div>
 
-            <!--INICIO opciones de icono cuadrado -->
-            <div class="modal fade apps-modal" id="appsModal" tabindex="-1" role="dialog"
-                aria-labelledby="appsModalLabel" aria-hidden="true" data-backdrop="false">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><i
-                        class="ik ik-x-circle"></i></button>
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="quick-search">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-4 ml-auto mr-auto">
-                                        <div class="input-wrap">
-                                            <input type="text" id="quick-search" class="form-control"
-                                                placeholder="Search..." />
-                                            <i class="ik ik-search"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-body d-flex align-items-center">
-                            <div class="container">
-                                <div class="apps-wrap">
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-mail"></i><span>Message</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-users"></i><span>Accounts</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-shopping-cart"></i><span>Sales</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-briefcase"></i><span>Purchase</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-server"></i><span>Menus</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-clipboard"></i><span>Pages</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-message-square"></i><span>Chats</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-map-pin"></i><span>Contacts</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-box"></i><span>Blocks</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-calendar"></i><span>Events</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-bell"></i><span>Notifications</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-pie-chart"></i><span>Reports</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-layers"></i><span>Tasks</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-edit"></i><span>Blogs</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-settings"></i><span>Settings</span></a>
-                                    </div>
-                                    <div class="app-item">
-                                        <a href="#"><i class="ik ik-more-horizontal"></i><span>More</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
-            </div>
-            <!--FIN opciones de icono cuadrado -->
+            </div>          
 
 
             <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
